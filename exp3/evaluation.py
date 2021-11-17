078899218424ba51d0fcb987b35cb3f0643f400c
Liesy <preposterous@163.com>
2021-11-17 15:49:49 +0800

    feat: 11.17 MAP, MRR and NDCG evaluations

--- exp3/evaluation.py
+++ exp3/evaluation.py
@@ -1,42 +1,112 @@
-import math
 import numpy as np
+from math import *
 
 
 def process_data(file):
     """
     - Output Format: 
-        - tweetid_gain: {query_id:{tweet_id: gain, tweet_id: gain, ...}}
-        - results: {query_id:[tweet_id, tweet_id, ...]}
+        - relevant_gain_dict:
+            - {query_id:{tweet_id: gain, tweet_id: gain, ...}}
+            - all relevant docs which gain > 0 for query
+        - retrival_dict:
+            - {query_id:[tweet_id, tweet_id, ...]}
+            - retrieved docs according to query
     """
-    gain_dict, res_dict = {}, {}
+    relevant_gain_dict, retrival_dict = {}, {}
 
     with open(file, 'r') as f:
         for line in f:
             info = line.strip().split(' ')
 
-            if info[0] not in gain_dict:  # info[0]=query_id
-                gain_dict[info[0]] = {}
-            if info[0] not in res_dict:
-                res_dict[info[0]] = []
+            if info[0] not in relevant_gain_dict:  # info[0]=query_id
+                relevant_gain_dict[info[0]] = {}
+            if info[0] not in retrival_dict:
+                retrival_dict[info[0]] = []
 
-            res_dict[info[0]].append(info[2])  # info[2]=tweet_id
+            retrival_dict[info[0]].append(info[2])  # info[2]=tweet_id
 
-            if int(info[3]) > 0:  # info[3]=gain
-                gain_dict[info[0]][info[2]] = int(info[3])
+            if int(info[3]) > 0:  # info[3]=relevant
+                relevant_gain_dict[info[0]][info[2]] = int(info[3])
 
-    return gain_dict, res_dict
+    return relevant_gain_dict, retrival_dict
 
 
-def MAP_eval(gain_dict, res_dict, k=100):
+def MAP_eval(relevant_gain_dict, retrival_dict, k=100):
     '''Mean Average Precision'''
-    pass
+    APs = []
+    for query in relevant_gain_dict.keys():
+        retrival_list = retrival_dict[query]
+        if len(retrival_list) <= 0:
+            print('query[', query, '] has no docs retrived.')
+            return []
+
+        truth = set(relevant_gain_dict[query].keys())
+        precision = []
+        i = 0
+        correct_num = 0
+        for doc in retrival_list:
+            i += 1
+            if doc in truth:
+                correct_num += 1
+                precision.append(correct_num / i)
+
+        if precision:
+            ap = np.sum(precision) / len(truth)
+            APs.append(ap)
+        else:
+            APs.append(0)
+
+    return np.mean(APs)
 
 
-def MRR_eval(gain_dict, res_dict, k=100):
+def MRR_eval(relevant_gain_dict, retrival_dict, k=100):
     '''Mean Reciprocal Rank'''
-    pass
+    RRs = []
+    for query in relevant_gain_dict.keys():
+        retrival_list = retrival_dict[query]
+        if len(retrival_list) <= 0:
+            print('query[', query, '] has no docs retrived.')
+            return []
+
+        truth = set(relevant_gain_dict[query].keys())
+        res = []
+        i = 0
+        for doc in retrival_list:
+            i += 1
+            if doc in truth:
+                res.append(1 / i)
+                break
+
+        if res:
+            rr = np.sum(res) / 1.0
+            RRs.append(rr)
+        else:
+            RRs.append(0)
+
+    return np.mean(RRs)
 
 
-def NDCG_eval(gain_dict, res_dict, k=100):
+def NDCG_eval(relevant_gain_dict, retrival_dict, k=100):
     '''Normalized Discounted Cumulative Gain'''
-    pass
+    NDCGs = []
+    for query in relevant_gain_dict.keys():
+        retrival_list = retrival_dict[query]
+        if len(retrival_list) <= 0:
+            print('query[', query, '] has no docs retrived.')
+            return []
+
+        truth_gain = sorted(set(relevant_gain_dict[query].values()), reverse=True)
+        i = 1
+        dcg, idcg = 0.0, 0.0
+        for doc in retrival_list:
+            i += 1
+            rel = relevant_gain_dict[query].get(doc, 0)
+            dcg += rel / log(i, 2)
+            idcg += truth_gain[i - 2] / log(i, 2)
+        
+        rel_1 = relevant_gain_dict[query].get(retrival_list[0], 0)
+        dcg += rel_1
+        ndcg = dcg / idcg
+        NDCGs.append(ndcg)
+
+    return np.mean(NDCGs)
--- exp3/main.py
+++ exp3/main.py
@@ -7,10 +7,10 @@ def main():
     parser.add_argument('--path', type=str, default='../dataset/qrels2014.txt')
     args = parser.parse_args()
 
-    gain_dict, res_dict = process_data(args.path)
-    MAP = MAP_eval(gain_dict, res_dict, 100)
-    MRR = MRR_eval(gain_dict, res_dict, 100)
-    NDCG = NDCG_eval(gain_dict, res_dict, 100)
+    relevant_gain_dict, retrival_dict = process_data(args.path)
+    MAP = MAP_eval(relevant_gain_dict, retrival_dict, 100)
+    MRR = MRR_eval(relevant_gain_dict, retrival_dict, 100)
+    NDCG = NDCG_eval(relevant_gain_dict, retrival_dict, 100)
 
     print('MAP = ', round(MAP,3), sep='')
     print('MRR = ', round(MRR,3), sep='')
